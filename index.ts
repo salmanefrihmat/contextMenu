7f0363bd7774e2d8113a8a9bc6b5691e5a2fa0d3
salmanefrihmat <sam.sensation@hotmail.fr>
2021-08-10 16:15:53 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,51 +1,18 @@
-import {
-  TreeGrid,
-  Resize,
-  Sort,
-  ContextMenu,
-  Edit,
-  Page
-} from '@syncfusion/ej2-treegrid';
+import { TreeGrid, RowDD,Reorder, Filter, Page } from '@syncfusion/ej2-treegrid';
 import { sampleData } from './datasource.ts';
-
-TreeGrid.Inject(Resize, Sort, Edit, ContextMenu, Page);
+TreeGrid.Inject(Page,Reorder, Filter,RowDD);
 
 let treeGridObj: TreeGrid = new TreeGrid({
   dataSource: sampleData,
   childMapping: 'subtasks',
-  allowSorting: true,
-  allowResizing: true,
-  allowPaging: true,
-  pageSettings: { pageSize: 7 },
-  editSettings: {
-    allowEditing: true,
-    allowAdding: true,
-    allowDeleting: true,
-    mode: 'Row'
-  },
-  contextMenuItems: [
-    'AutoFit',
-    'AutoFitAll',
-    'SortAscending',
-    'SortDescending',
-    'Edit',
-    'Delete',
-    'Save',
-    'Cancel',
-    'FirstPage',
-    'PrevPage',
-    'LastPage',
-    'NextPage'
-  ],
   treeColumnIndex: 1,
+  allowRowDragAndDrop: true,
+  allowReordering: true,
+  allowPaging: true,
+  allowFiltering: true,
+  pageSettings: { pageSize: 15 },
   columns: [
-    {
-      field: 'taskID',
-      headerText: 'Task ID',
-      isPrimaryKey: true,
-      width: 90,
-      textAlign: 'Right'
-    },
+    { field: 'taskID', headerText: 'Task ID', width: 90, textAlign: 'Right' },
     {
       field: 'taskName',
       headerText: 'Task Name',
@@ -56,18 +23,11 @@ let treeGridObj: TreeGrid = new TreeGrid({
       field: 'startDate',
       headerText: 'Start Date',
       width: 90,
-      editType: 'datePickeredit',
       textAlign: 'Right',
       type: 'date',
       format: 'yMd'
     },
-    {
-      field: 'duration',
-      headerText: 'Duration',
-      width: 80,
-      editType: 'numericedit',
-      textAlign: 'Right'
-    }
+    { field: 'duration', headerText: 'Duration', width: 80, textAlign: 'Right' }
   ]
 });
 
